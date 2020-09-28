5c34f0b2a3b39663a7c7782c4ea6872ae5ad9652
Mightyrey <tobias.hartmann297@gmail.com>
2020-09-28 21:23:00 +0200

    Seite anzeigen

--- Server/app.js
+++ Server/app.js
@@ -3,7 +3,7 @@
 const express = require("express"); 
 const mysql = require("mysql");
 const path = require('path');
-
+const http =require('http');
 // Start des Servers
 const app = express();
 
@@ -33,12 +33,57 @@ db.connect((error)=>{
 
 // Routen
 
+// Hier kommt das Frontend rein/ __dirname gibt dir zugriff auf die aktuelle Direktory bei uns dann eins hoch springen!
+const publicDirectory = path.join(__dirname, './Client');
+// gibt das akutlle direktory zurück
+//console.log(__dirname);
+
+// hier wird die zuvor erzeuge Direktory benutzt, siehe oben const publicDirectory
+app.use(express.static(publicDirectory));
+
+
+//Parse URL-encoded bodies (as sent by html from), nimmt die von HTML gesendeten Sachen entgegen
+app.use(express.urlencoded({extended:false}));
+// Sorg dafür das die Daten die kommen im Format JSON übertragen werden // Parse JSON Bodies (as sent by API Clients)
+app.use(express.json());
+
+
+
+
+// Starten der HTMl Engine
+app.set('index', 'html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //app.use('/',require('./routes/pages.js'));
 
-app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname + '/Client/index.html'));
-});
-	
+
+
+//app.get('/', function(req,res){
+  //  res.sendFile(path.join(__dirname + '/Client/index.html'));
+//});
+
+
 //app.get('/', (req, res) => {
   //  res.render('/Client/index');
   //});
