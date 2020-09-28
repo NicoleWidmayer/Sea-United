6c37d3ceafda123e332dcc480bd80b80d5502cf6
Mightyrey <tobias.hartmann297@gmail.com>
2020-09-28 21:28:54 +0200

    Index Seite anzeigen

--- Server/app.js
+++ Server/app.js
@@ -31,62 +31,30 @@ db.connect((error)=>{
 });
 
 
-// Routen
-
+// Anzeigen der Frontend Seite
 // Hier kommt das Frontend rein/ __dirname gibt dir zugriff auf die aktuelle Direktory bei uns dann eins hoch springen!
 const publicDirectory = path.join(__dirname, './Client');
-// gibt das akutlle direktory zurück
-//console.log(__dirname);
 
 // hier wird die zuvor erzeuge Direktory benutzt, siehe oben const publicDirectory
 app.use(express.static(publicDirectory));
-
-
-//Parse URL-encoded bodies (as sent by html from), nimmt die von HTML gesendeten Sachen entgegen
-app.use(express.urlencoded({extended:false}));
-// Sorg dafür das die Daten die kommen im Format JSON übertragen werden // Parse JSON Bodies (as sent by API Clients)
-app.use(express.json());
-
-
-
 
 // Starten der HTMl Engine
 app.set('index', 'html')
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//Routen
 
 //app.use('/',require('./routes/pages.js'));
-
-
 
 //app.get('/', function(req,res){
   //  res.sendFile(path.join(__dirname + '/Client/index.html'));
 //});
 
-
 //app.get('/', (req, res) => {
   //  res.render('/Client/index');
   //});
+
+
 
 // Auf diesen Port hört der express Server! Wichtig 
 app.listen(5000,() =>{
