a9df308e756552640a67bb129839f7943d274478
Melissa Negele <melissanegele@yahoo.de>
2020-09-29 14:38:40 +0200

    ausflug

--- Server/Client/ausflug.js
+++ Server/Client/ausflug.js
@@ -6,7 +6,7 @@ function loadHTMLTable(data){
     const table = document.querySelector('table tbody');
 
     if(data.length === 0){
-        table.innerHTML="<tr> <td class='no-data' colspan='5'> No Data</td> </tr>";
+        table.innerHTML="<tr> <td class='no-data' colspan='3'> No Data</td> </tr>";
 
     }
 }
