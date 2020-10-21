340ee13114cb8b79e1c12be6b9080e612ade7a6f
BeekeWiltfang <wi19253@lehre.dhbw-stuttgart.de>
2020-10-21 19:44:53 +0200

    upgrade fetch methods with try catch

--- Server/public/ausflug.js
+++ Server/public/ausflug.js
@@ -15,16 +15,30 @@ function RowAusflug(boot, kategorie, kapazität, datum, preis) {
 
 
 
-  function insertRow() {
-      $ ("#ausflugTable tbody").append("<tr>" + 
-      "<td>" + ausflug.boot + "</td>" +
-      "<td>" + ausflug.kategorie + "</td>" +
-      "<td>" + ausflug.kapazität + "</td>" +
-      "<td>" + ausflug.datum + "</td>" +
-      "<td>" + ausflug.preis + "</td>" +
-      "<td><button>data-id=" + ausflug.boot + "</button></td>" +
-      "</tr>")
-  }
+  function loadHTMLTable(data){
+    const table = document.querySelector('table tbody');
+
+    //Wenn keine Daten vorliegen greift dies
+    if(data.length === 0){
+        table.innerHTML="<tr> <td class='no-data' colspan='6'>Derzeit sind keine Termine verfügbar.</td> </tr>";
+        return;
+    }
+    let tableHTML="";
+
+    //Hierdurch wird die Tabelle mit den Daten zeilenweise befüllt
+    data.forEach(function({Boot, Kategorie, Kapazität, Datum, Preis, Buchen}){
+        tableHTML += "<tr>";
+        tableHTML += `<td>§{boot}<td>`;
+        tableHTML += `<td>§{kategorie}<td>`;
+        tableHTML += `<td>§{kapazität}<td>`;
+        tableHTML += `<td>§{datum}<td>`;
+        tableHTML += `<td>§{preis}<td>`;
+        tableHTML += `<td><button class="dieseZeileBuchen" data-id=§{boot}>Buchen</button><td>`;
+        tableHTML += "</tr>";
+    });
+
+    table.innerHTML = tableHTML;
+}
 
 
 
@@ -50,7 +64,7 @@ const ladeAusfluege = () => {
         tbody.innerHTML = ''; //tabelle wird geleert
         ausflug.forEach((ausflug) => {
           let ausflugROW = new RowAusflug(ausflug.boot, ausflug.kategorie, ausflug.kapazität, ausflug.datum, ausflug.preis);
-          insertRow(ausflugROW);
+          loadHTMLTable(ausflugROW);
         })
       }).catch((e) => {
         alert(`Fehler ${e}`);
