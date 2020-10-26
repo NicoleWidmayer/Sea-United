a99f41f8e496c8b40140845d9dde203f9e531855
BeekeWiltfang <wi19253@lehre.dhbw-stuttgart.de>
2020-10-26 14:40:46 +0100

    style stuff

--- Server/public/termin.js
+++ Server/public/termin.js
@@ -1,226 +1,160 @@
 // Termin Skript
 // FIXME: Beschreibung des Termin Skripts
 
-
-// Übertragen der Kennung der Botte in einen Array 
+// give "Kennung" into Array 
 let kennungObject = [];
 fetch("/termineDropDown").then((res) => {
     if (!res.ok) return Promise.reject(res.status);
   
     return res.json();
-  }).then((objekte) => {
-    objekte.forEach((objekte) => {
+}).then((objekte) => {
+        objekte.forEach((objekte) => {
         kennungObject.push(objekte);
     })
-  });
+});
 
-
-
-// Event Listener auf die Webseite
+// Event Listener for Webseite
 document.addEventListener('DOMContentLoaded', function (e) {
     fetch('/termineAll')
     .then( async res =>{
         const data = await res.json(); 
         loadHTMLTable(data);
 
+        // Event Listener for button "Termin"
+        const fetchRegButton = document.querySelector("#termin-btn");
 
-// Event Listener auf den Button Termin hinzufügen
-const fetchRegButton = document.querySelector("#termin-btn");
+        fetchRegButton.addEventListener("click", (reg) => {
+            let datum = document.querySelector("#gebdat").value;
+            let kennung = document.querySelector("#kennung").value;
 
-fetchRegButton.addEventListener("click", (reg) => {
+            let terminData = {
+                kennung: kennung,
+                datum: datum,
+            }
 
-   // let kennung = document.querySelector("#kennung").value;
-    let datum = document.querySelector("#gebdat").value;
-    let kennung = document.querySelector("#kennung").value;
+            // load data into database with POST
+            fetch("/erstellen", {
+                method: "Post",
+                body: JSON.stringify(terminData),
+                headers: {
+                    "content-type": "application/json",
+                },
+            }).then((res) => {
+                // wird aktuell nicht ausgeführt
+                if(res.status >=400) {
+                    console.log("Status 200");
+                    alert("Termin erfolgreich angelegt");
+                } else {
+                    alert("Termin konnte nicht angelegt werden");
+                }
+            }) // End of fetch then
+        }) // End of Event Listener
 
-    let terminData = {
-        kennung: kennung,
-        datum: datum,
-    }
-    console.log(terminData);
-   // Über Post in die Datenbank laden
-   fetch("/erstellen", {
-    method: "Post",
-    body: JSON.stringify(terminData),
-    headers: {
-        "content-type": "application/json",
-    },
-}).then((res) => {
-  // wird aktuell nicht ausgeführt
-    if(res.status >=400 )
-    {
-      console.log("Status 200");
-      alert("Termin erfolgreich angelegt");
-    }
-    else {
-        alert("Termin konnte nicht angelegt werden");
-    }
-}) // fetch then
-}) // Event Listener
+        // DropDown Menü
+        /////////////////// Copyright-Vermerk /////////////////// 
+        // https://www.w3schools.com/howto/howto_js_cascading_dropdown.asp
+        const kenjectSel = document.getElementById("kennung");
+        for(i=0; i< kennungObject.length; i++) {
+            kenjectSel.options[kenjectSel.options.length] = new Option(kennungObject[i]['kennung']);   
+        }     
+    }) // then Fetch Website loaded    
+}); // End of load Website
 
-
-
-
-// DropDown Menü
-/////////////////// Copyright-Vermerk /////////////////// 
-// https://www.w3schools.com/howto/howto_js_cascading_dropdown.asp
-    const kenjectSel = document.getElementById("kennung");
-    
-    for(i=0; i< kennungObject.length; i++)
-    {
-    kenjectSel.options[kenjectSel.options.length] = new Option(kennungObject[i]['kennung']);   
-    }     
-  
-
-    }) // then Fetch Webseite geladen     
-}); // Webseite geladen zu ende
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Event Listener wenn etwas innerhalb von tbody gedrückt wird zum löschen und bearbeiten
-
+// Event Listener for the case that somethin within tbody was pused. To DELETE or to EDIT.
 document.querySelector('table tbody').addEventListener('click', function(event) {
-    // Falls delete gedrückt wird
+    // If DELETE is pushed...
     if (event.target.className === "delete-row") {
         console.log(event.target.dataset.id);
         
-     // Meldung inspiriert durch: https://www.w3schools.com/js/js_popup.asp
-     // Hier wird eine bestätigung zum Buchen abgefragt
-  
-    if (confirm("Wollen Sie diesen Termin wirklich löschen?")) {
-      console.log("OK");
-      console.log()
-      deleteRowById(event.target.dataset.id);
-    } else {
-      console.log("Abbrechen")
-    }
-    } // Falls Edit gedrückt wird
+        // Meldung inspiriert durch: https://www.w3schools.com/js/js_popup.asp
+        // Asks for a confirmation to delete
+        if (confirm("Wollen Sie diesen Termin wirklich löschen?")) {
+            deleteRowById(event.target.dataset.id);
+        } else {
+            console.log("Abbrechen")
+        }
+    } // If EDIT is pushed...
     if (event.target.className === "edit-row") {
         handleEditRow(event.target.dataset.id);
     }
 });
 
-
-
-
-
-
-// Funktion zum sichbarmachen der Termine, sobald bearbeiten gedrückt wurde
+// Function to show meetings, when edit is pushed
 function handleEditRow(id) {
    
-    // Felder auf sichtbar setzen
+    // Set Fields visible
     const updateSection = document.querySelector('#update-row');
     updateSection.hidden = false;
 
     // Feldern Werte zuweißen FIXME: aktuell noch ohne funktion
-  //  document.querySelector('#update-gebucht').dataset.id = datum;
-  //  document.querySelector('#update-gebdat').dataset.id = gebucht;
     const kenjectSel = document.getElementById("update-kennung");
     for(i=0; i< kennungObject.length; i++)
     {
     kenjectSel.options[kenjectSel.options.length] = new Option(kennungObject[i]['kennung']);   
     }     
+  
+    // Event Listener auf den Button Update
+    const fetchUpdateButton = document.querySelector("#update-btn");
 
+    fetchUpdateButton.addEventListener("click", (reg) => {
+        let datum = document.querySelector("#update-gebdat").value;
+        let kennung = document.querySelector("#update-kennung").value;
+        let gebucht = document.querySelector("#update-gebucht").value;
 
-    
-// Event Listener auf den Button Update
-const fetchUpdateButton = document.querySelector("#update-btn");
+        let terminUpdateData = {
+            kennung: kennung,
+            datum: datum,
+            gebucht: gebucht,
+            id: id
+        }
+        console.log(terminUpdateData);
+        // Bring data with PATCH into the database
+        fetch("/UpdateTermin", {
+            method: "PATCH",
+            body: JSON.stringify(terminUpdateData),
+            headers: {
+                "content-type": "application/json",
+            },
+        }).then((res) =>{
+            if(res.ok){
+            location.reload();
+            }
+            else{
+                alert("Termin konnte nicht bearbeitet werden");
+            }
+        })// End of Fetch Then
+    }) // End of Event Listener
+}// End of Edit Listener
 
-fetchUpdateButton.addEventListener("click", (reg) => {
-
-   // let kennung = document.querySelector("#kennung").value;
-    let datum = document.querySelector("#update-gebdat").value;
-    let kennung = document.querySelector("#update-kennung").value;
-    let gebucht = document.querySelector("#update-gebucht").value;
- 
-    let terminUpdateData = {
-        kennung: kennung,
-        datum: datum,
-        gebucht: gebucht,
-        id: id
-    }
-
-    console.log(terminUpdateData);
-   // Über Post in die Datenbank laden
-   fetch("/UpdateTermin", {
-    method: "PATCH",
-    body: JSON.stringify(terminUpdateData),
-    headers: {
-        "content-type": "application/json",
-    },
-}).then((res) =>{
-    if(res.ok){
-      location.reload();
-    }
-    else{
-        alert("Termin konnte nicht bearbeitet werden");
-    }
-    
-
-})// Fetch Then
-}) // Event Listener
-
-
-
-}// Edit Listener
-
-
-
-
-
-
-
-
-
-
-// Funktion Löschen
-
+// Delete function
 function deleteRowById(id) {
     fetch('/delete/'+ id, {
         method: 'DELETE',
     })
     .then((res) =>{
-        
         if(res.ok){
             location.reload();
         }
         else{
             console.log("Termin Löschen war nicht erfolgreich");
-        }
-        
+        }   
     })
 }
 
-
-
-
-
-
-// Funktion zum anzeigen der Tabelle
+// functions to show the filled table
 /////////////////// Copyright-Vermerk /////////////////// 
 // https://www.youtube.com/watch?v=vrj9AohVhPA
-
 function loadHTMLTable(data) {
     const table = document.querySelector('table tbody');
-    // Rückgabe, falls das ausgelesene Result leer ist
+    // In case that the ResultSet is empty
     if (data.length === 0) {
         table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
         return;
     }
 
-    // Das hier passiert mit der Tabelle wenn das Result nicht leer ist
+    // It´s to not have doubble information in table
     let tableHtml = "";
-
+    // Table will be filled
     data.forEach(function ({kennung, preis, kapazität, kategorie, datum, gebucht, ID}) {
         tableHtml += "<tr>";
         tableHtml += `<td>${ID}</td>`;
