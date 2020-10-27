f3f1d8d65b1b32cc7a0b9ecb355a2640a2896ba7
Mightyrey <tobias.hartmann297@gmail.com>
2020-10-27 16:53:15 +0100

    Kommentare register.js

--- Server/public/register.js
+++ Server/public/register.js
@@ -1,33 +1,33 @@
+/*
+Author: Group Sea_United
+Version: 1.0
+Description: 
+Registration, here new users are created in the database
+*/
 
+
+// Button Declaration
 const fetchRegButton = document.querySelector("#submitReg");
 
+// Event Listener for the button register
 fetchRegButton.addEventListener("click", (reg) => {
-    console.log("Registrierung Skript wird aufgerufen");
-    // Testn ob dies im Fehlerfall noch benötigt wird
-  //  reg.preventDefault();
-
+   
     let usename = document.querySelector("#pwBenutzername1").value;
     let passwort = document.querySelector("#pwPasswort1").value;
     let passwortVerify = document.querySelector("#pwPasswort2").value;
     let email = document.querySelector("#Email").value;
 
-    console.log(usename);
-    console.log(passwort);
-    console.log(passwortVerify);
-    console.log(email);
-
-    // Daten für JSON aufbereiten
-
+    //Saving the website data in an object
     let userData = {
         benutzername: usename,
         passwort: passwort,
         email: email,
     }
    
-    // Prüfen ob passwort übereinstimmt
+    // Check if the password matches
     if (passwort == passwortVerify) {
         
-        // Über Post in die Datenbank laden
+        //Load data into database
         fetch("/register", {
             method: "Post",
             body: JSON.stringify(userData),
@@ -35,9 +35,8 @@ fetchRegButton.addEventListener("click", (reg) => {
                 "content-type": "application/json",
             },
 
-
         }).then((res) => {
-          // wird aktuell nicht ausgeführt
+           // Output of the message if the new user is created or there is a problem with the registration
             if(res.ok)
             {
               alert("Registrierung erfolgreich");
@@ -45,12 +44,9 @@ fetchRegButton.addEventListener("click", (reg) => {
             else {
                 alert("Registrierung nicht erfolgreich");
             }
-
-
         })
-
     }
-    // Falls Passwort nicht überinstimmt
+     // Output of the message If the passwords do not match
     else{
         alert("Passwörter stimmen nicht überein");
 
